aaa246fae0fbbeb6745113ab7210b6a0119cd44a
opedia <hannan.opedia@gmail.com>
2022-06-27 16:33:25 +0600

    some bug fixed

--- js/script.js
+++ js/script.js
@@ -402,47 +402,53 @@ jQuery(document).ready(function() {
 
     $('.course-sidebar').slick({
         vertical: true,
-        verticalSwiping: true,
         slidesToShow: 8,
         slidesToScroll: 1,
         asNavFor: '.course-content',
         dots: false,
-        // autoplay: true,
+        autoplay: true,
         arrows: false,
         lazyLoad: 'ondemand',
         centerMode: true,
-        focusOnSelect: true
-    });
+        focusOnSelect: true,
 
+    });
 
     $('.course-content').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         arrows: false,
         fade: false,
-        // autoplay: true,
+        autoplay: true,
+        dots: false,
         asNavFor: '.course-sidebar',
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                     infinite: true,
-                    dots: true
+                    dots: true,
+                    arrows: false,
+                    dots: false,
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
+                    arrows: false,
                     slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToScroll: 2,
+                    dots: false
                 }
             },
             {
                 breakpoint: 575,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: false,
                 }
             }
         ]
